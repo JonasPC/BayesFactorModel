9acb5eb4904dc198cc8d7b5bb7cd4130e0d5b253
Jonas Philip Christensen <jonas@Jonass-MacBook-Air.local>
2018-09-20 20:48:46 +0200

    Still working on initial phase of Gibbs sampler

--- model/gibbssampler.py
+++ model/gibbssampler.py
@@ -1,22 +1,30 @@
 
-class GibbsSampler(self, data, """hyperparameters, starting_values"""):
+class GibbsSampler(self, data, number_of_factors=3):
 
     """
-    Samples parameters of posterior given data and hyper parametersself.
+    Samples parameters of posterior given data and hyper parameters.
 
     ===
     - Parameters -
     data: Must be a df of dimension (N x p)
-    B: Hyper parameter of the var/covar matrix (scalar)
-    v: Hyper parameter of the var/covar matrix (scalar)
-    m: Hyper parameter of Lambda (scalar)
-    H: Hyper parameter of Lambda (scalar or matrix)
-    lambda_0: starting value of lambda (scalar or matrix)
-    F_0: starting value of F (scalar or vector of dimension m)
-
+    number_of_factors: must be a series
     """
+    import numpy as np
 
     def __init__(self):
         self.data = data
-        self.n = len(data)
-        self.p = "something"
+        self.n = len(data) #num of observations
+        self.k = len(self.data.columns) #num of covariates
+        self.p = len(number_of_factors) #num of factors
+
+    def cov_matrix(self, type='identity'):
+        """
+        Sets the cov_matrix to a fixed identity matrix or allows it to
+        be simulated.
+        """
+        if type == 'identity':
+            self.cov = np.identity(self.n)
+        elif type == 'simulate':
+            self.cov = pd.DataFrame(np.ones((self.n, self.n)))
+        else:
+            raise Exception("type must be 'identity' or 'simulate'")
